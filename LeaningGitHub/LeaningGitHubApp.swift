b3abe0342001b8330505c7d84afb0c79c98e3b85
Ken Dice <ken.dice@cookchildrens.org>
2021-07-27 15:57:22 -0500

    Initial Commit

---
+++ LeaningGitHub/LeaningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaningGitHubApp.swift
+//  LeaningGitHub
+//
+//  Created by Ken Dice on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct LeaningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
